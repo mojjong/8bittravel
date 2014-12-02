de5dbbf02c350189b7ed68f6f1182432b879e5a8
Ohgahyeon <3summer61@gmail.com>
2014-12-02 09:22:46 +0900

    placeForm

--- src/main/java/org/yo/web/GuideBbsController.java
+++ src/main/java/org/yo/web/GuideBbsController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.yo.guidebbs.service.GuideBbsService;
 import org.yo.guidebbs.vo.GuideBbsVO;
 import org.yo.travelbbs.vo.TravelbbsVO;
+
 
 @Controller
 @RequestMapping("/bbs/guide/*")
@@ -25,22 +27,15 @@ public class GuideBbsController {
 	
 	@Inject
 	GuideBbsService service;
-	
-/*	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public @ResponseBody List<GuideBbsVO> glist(Integer no, Model model) {
-		//model.addAttribute("guideList", service.glist(no));
-		return service.glist(no);
-	}
 
-	*/
 
 	//글 리스트 뿌리기(무한스크롤)
-		@RequestMapping(value="/list", method = RequestMethod.GET)
-		public String scrollList1(){
-			System.out.println("스크롤");
-			return "/bbs/travelbbs/guideboard";
-		}
-		
+	@RequestMapping(value="/list", method = RequestMethod.GET)
+	public String scrollList1(){
+		System.out.println("스크롤");
+		return "/bbs/guidebbs/guideboard";
+	}
+	
 		
 	//글 리스트 뿌리기(무한스크롤)
 		@RequestMapping(value="/list", method = RequestMethod.POST,produces = "application/json")
@@ -52,6 +47,22 @@ public class GuideBbsController {
 		
 			return list;
 		}
+		
+		@RequestMapping(value="/place", method = RequestMethod.GET)
+		public String placeAdd(){
+			System.out.println("스크롤");
+			return "/bbs/guidebbs/guideboard";
+		}
+		//장소 추가하기
+		@RequestMapping(value = "/place", method = RequestMethod.POST)
+			public void placeAdd(GuideBbsVO vo){
+			System.out.println("aaaa");
+				service.placeAdd(vo);
+				logger.info("컨트롤 : " + vo.toString());
+		
+			}
+			
+
 
 }
 
